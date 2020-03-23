907c6c7520cf3e3fa47599c2e9ce71bb8bc5ce62
Rhys Merritt <rhys@Rhyss-MacBook-Pro.local>
2020-03-23 17:41:33 +0000

    put all the styling in place, and the jQuery lightbox with customisation

---
+++ js/fancybox.js
@@ -0,0 +1,12 @@
+$('[data-fancybox="gallery"]').fancybox({
+	loop:true,
+	buttons: [
+		//"zoom",
+		//"share",
+		//"slideShow",
+		//"fullScreen",
+		//"download",
+		//"thumbs",
+		"close"
+	  ],
+});
